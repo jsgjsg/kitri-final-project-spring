03f0114513a7f9ff3941cb20d3bd6757346a0f4d
Jeon Seung Kyu <jsgcys0815@gmail.com>
2024-07-29 10:30:04 +0900

    Feat: "MySQL과 연결"
    MySQL과 연결 성공

---
+++ src/main/java/com/kitri/bark_meow_party_server/domain/Test.java
@@ -0,0 +1,17 @@
+package com.kitri.bark_meow_party_server.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Test {
+    private long id;
+    private String testcol1;
+    private String testcol2;
+    private String testcol3;
+}
