562aa00709f76b7e85d0b4219de5f458a03c9b91
miwoul <pjh200305@gmail.com>
2024-07-31 17:55:03 +0900

    Chore: "주석처리"
    후기, 후기댓글, 피드 댓글 관련 모든 것 주석 처리 완료

--- src/main/java/com/kitri/bark_meow_party_server/mapper/FeedCommentMapper.java
+++ src/main/java/com/kitri/bark_meow_party_server/mapper/FeedCommentMapper.java
@@ -7,14 +7,24 @@ import java.util.List;
 
 @Mapper
 public interface FeedCommentMapper {
+
+    //모든 피드에 대한 댓글 조회
     @Select("SELECT * FROM feed_comment")
     List<FeedComment> selectAll();
+
+    //주어진 ID에 해당하는 피드에 대한 댓글 조회
     @Select("SELECT * FROM feed_comment WHERE id = #{id}")
     FeedComment selectById(Long id);
+
+    //피드에 대한 댓글 추가
     @Insert("INSERT INTO FeedComment(feed_id, content, create_at) VALUES (#{feed_id}, #{content}, #{create_at})")
     void feedCommentInsert(FeedComment comment);
+
+    //주어진 ID에 해당하는 피드에 대한 댓글 수정
     @Update("UPDATE FeedComment SET content=#{content} WHERE id=#{id}")
     void feedCommentUpdate(FeedComment comment);
+
+    //주어진 ID에 해당하는 피드에 대한 댓글 삭제
     @Delete("DELETE FROM feed_comment WHERE id = #{id}")
     void deleteFeedComment(Long id);
 }
