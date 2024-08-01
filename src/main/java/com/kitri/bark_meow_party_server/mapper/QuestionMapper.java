f46916e4e5ecc995c7ccc15d080910f440124261
miwoul <pjh200305@gmail.com>
2024-08-01 16:58:40 +0900

    Feat: "QnA의 qa의 Read"
    QnA에서 qa의 Read구현

---
+++ src/main/java/com/kitri/bark_meow_party_server/mapper/QuestionMapper.java
@@ -0,0 +1,2 @@
+package com.kitri.bark_meow_party_server.mapper;public interface QuestionMapper {
+}
---
+++ src/main/java/com/kitri/bark_meow_party_server/service/AnswerService.java
@@ -0,0 +1,2 @@
+package com.kitri.bark_meow_party_server.service;public class AnswerService {
+}
