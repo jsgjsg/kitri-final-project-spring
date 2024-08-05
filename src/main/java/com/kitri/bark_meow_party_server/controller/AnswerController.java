d6c48ad3d6165441d5ae164dba180b876399f372
miwoul <pjh200305@gmail.com>
2024-08-05 11:12:36 +0900

    Feat: "의사 QnA  Answer기능"
    의사 QnA의 Answer 기능만 구현완료, 테스트는 아직

--- src/main/java/com/kitri/bark_meow_party_server/controller/AnswerController.java
+++ src/main/java/com/kitri/bark_meow_party_server/controller/AnswerController.java
@@ -1,9 +1,37 @@
 package com.kitri.bark_meow_party_server.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.kitri.bark_meow_party_server.domain.Answer;
+import com.kitri.bark_meow_party_server.service.AnswerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/qa")
 public class AnswerController {
+    @Autowired
+    private AnswerService answerService;
+
+    @PostMapping("/{qaId}/answers")
+    public Answer createAnswer(@PathVariable Long qaId, @RequestBody Answer answer) {
+        answer.setQaId(qaId);
+        answerService.addAnswer(answer);
+        return answer;
+    }
+    @GetMapping("/{qaId}/answers")
+    public List<Answer> getByQaId(@PathVariable Long qaId) {
+        return answerService.getByQaId(qaId);
+    }
+    @PutMapping("/{qaId}/answers/{answerId}")
+    public void updateAnswer(@PathVariable Long qaId, @PathVariable Long answerId, @RequestBody Answer answer) {
+        answer.setId(answerId);
+        answer.setQaId(qaId);
+        answerService.updateAnswer(answer);
+    }
+    @DeleteMapping("/{qaId}/answers/{answerId}")
+    public void deleteAnswer(@PathVariable Long qaId, @PathVariable Long answerId) {
+        answerService.deleteAnswer(answerId);
+    }
+
 }
--- src/main/java/com/kitri/bark_meow_party_server/service/AnswerService.java
+++ src/main/java/com/kitri/bark_meow_party_server/service/AnswerService.java
@@ -1,7 +1,46 @@
 package com.kitri.bark_meow_party_server.service;
 
+import com.kitri.bark_meow_party_server.domain.Answer;
+import com.kitri.bark_meow_party_server.domain.Doctor;
+import com.kitri.bark_meow_party_server.mapper.AnswerMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AnswerService {
+    @Autowired
+    AnswerMapper answerMapper;
+    @Autowired
+    DoctorService doctorService;
+
+    public List<Answer> getAnswers() {
+        answerMapper.getAll();
+        return answerMapper.getAll();
+    }
+    public Answer getAnswerById(Long id) {
+        return answerMapper.getById(id);
+    }
+    public List<Answer> getByQaId (Long qaId) {
+        return answerMapper.getByQaId(qaId);
+    }
+    public Answer addAnswer(Answer answer) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String doctorName = authentication.getName();
+
+        Doctor doctor = doctorService.findByDoctorname(doctorName);
+        answer.setDoctorId(doctor.getId());
+
+        answerMapper.createAnswer(answer);
+        return answer;
+    }
+    public void updateAnswer(Answer answer) {
+        answerMapper.updateAnswer(answer);
+    }
+    public void deleteAnswer(Long id) {
+        answerMapper.deleteAnswer(id);
+    }
 }
