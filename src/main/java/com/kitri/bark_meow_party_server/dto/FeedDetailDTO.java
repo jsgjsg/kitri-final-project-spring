876fb140a81703508ef949cd8c99c72fd2a2df32
Jeon Seung Kyu <jsgcys0815@gmail.com>
2024-08-07 17:39:20 +0900

    Feat: "피드에 해시태그 추가하여 구현 및 수정 완료하였음"

--- src/main/java/com/kitri/bark_meow_party_server/dto/FeedDetailDTO.java
+++ src/main/java/com/kitri/bark_meow_party_server/dto/FeedDetailDTO.java
@@ -1,10 +1,13 @@
 package com.kitri.bark_meow_party_server.dto;
 
 import com.kitri.bark_meow_party_server.domain.Feed;
+import com.kitri.bark_meow_party_server.domain.FeedHashtag;
 import com.kitri.bark_meow_party_server.domain.FeedLike;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -13,4 +16,5 @@ public class FeedDetailDTO {
     private FeedWithUserDTO feedWithUser;
     private int likeCount;
     private boolean isLiked;
+    private List<FeedHashtag> feedHashtags;
 }
