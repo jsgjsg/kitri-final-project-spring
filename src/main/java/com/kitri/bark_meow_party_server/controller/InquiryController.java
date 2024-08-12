8b4a9cf3e80a7934ea9cb8c303231c477d61ff95
miwoul <pjh200305@gmail.com>
2024-08-12 18:31:15 +0900

    Fix: "문의 추가 수정"
    최신화, 페이징

--- src/main/java/com/kitri/bark_meow_party_server/controller/InquiryController.java
+++ src/main/java/com/kitri/bark_meow_party_server/controller/InquiryController.java
@@ -1,12 +1,16 @@
 package com.kitri.bark_meow_party_server.controller;
 
 import com.kitri.bark_meow_party_server.domain.Inquiry;
+import com.kitri.bark_meow_party_server.domain.Review;
+import com.kitri.bark_meow_party_server.dto.InquiryWithUserDTO;
 import com.kitri.bark_meow_party_server.service.InquiryService;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/inquiry")
@@ -15,13 +19,28 @@ public class InquiryController {
     InquiryService inquiryService;
 
     @GetMapping("")
-    public List<Inquiry> getInquiry() {
+    public List<InquiryWithUserDTO> getInquiry() {
         return inquiryService.getAllInquiry();
     }
 
     @GetMapping("/{id}")
     public Inquiry getInquiryById(@PathVariable Long id) {
         return inquiryService.getInquiryById(id);
+    }
+
+    //페이징
+    @GetMapping("/page")
+    public Map<String, Object> getInquiryPage(
+            @RequestParam (value = "page", defaultValue = "1") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size) {
+        List<Inquiry> inquiry = inquiryService.getInquiry(page, size);
+        int total = inquiryService.getInquiryCount();
+        Map<String, Object> result = new HashMap<>();
+        result.put("inquiry", inquiry);
+        result.put("total", total);
+        result.put("page", page);
+        result.put("size", size);
+        return result;
     }
 
     @PostMapping("")
